bb0e857510d827224495068649438759530794f1
vikyw89 <vikyw89@gmail.com>
2023-11-26 00:21:43 +0800

    savepoint

--- server/main.py
+++ server/main.py
@@ -1,20 +1,38 @@
+import time
 from typing import Union
 from fastapi import FastAPI
 from fastapi.responses import StreamingResponse
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost:3000",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
-@app.get("/http_stream/{media_type}")
-def http_stream(media_type: str):
+@app.get("/api/http_stream")
+def http_stream():
     def generator():
         while True:
-            yield "test"
-    StreamingResponse(generator(), media_type=media_type)
+            yield  "test"
+            yield  "test2"
+            time.sleep(1)
+    return StreamingResponse(generator(), media_type="text/event-stream")
 
 
 @app.get("/items/{item_id}")
